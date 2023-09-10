3782f2a6e1e93f99ee77edaeabc31e2f192bcce3
Your Name <you@example.com>
2023-09-10 09:24:06 +0700

    todolist update 1

--- app/Todolist.tsx
+++ app/Todolist.tsx
@@ -1,17 +1,19 @@
 "use client"
 import React from 'react'
-import Image from 'next/image'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { AiTwotoneDelete, AiTwotoneEdit} from "react-icons/ai";
 
 type Props = {}
 
-const Todolist = () => {
-  const [todos, setTodos] = useState<string[]>([]);
+const Todolist: React.FC = () => {
+  const [todos, setTodos] = useState<{ text: string; completed: boolean }[]>([]);
   const [newTodo, setNewTodo] = useState<string>('');
+  const [editIndex, setEditIndex] = useState<number | null>(null);
+  const [editedText, setEditedText] = useState<string>('');
 
   const addTodo = () => {
     if (newTodo.trim() === '') return;
-    setTodos([...todos, newTodo]);
+    setTodos([...todos, { text: newTodo, completed: false }]);
     setNewTodo('');
   };
 
@@ -20,6 +22,39 @@ const Todolist = () => {
     newTodos.splice(index, 1);
     setTodos(newTodos);
   };
+
+  const toggleTodo = (index: number) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+  
+  const startEditing = (index: number) => {
+    setEditIndex(index);
+    setEditedText(todos[index].text);
+  };
+
+  const stopEditing = () => {
+    setEditIndex(null);
+  };
+
+  const updateTodo = (index: number) => {
+    const newTodos = [...todos];
+    newTodos[index].text = editedText;
+    setTodos(newTodos);
+    stopEditing();
+  };
+
+  useEffect(() => {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <div>
@@ -40,14 +75,49 @@ const Todolist = () => {
       </div>
       <ul>
         {todos.map((todo, index) => (
-          <li key={index} className="flex justify-between items-center mb-2 text-blue-500">
-            {todo}
-            <button
-              className="text-red-500"
-              onClick={() => removeTodo(index)}
-            >
-              Remove
-            </button>
+          <li key={index} className="flex justify-between items-center my-2 p-4 text-blue-500 bg-sky-200 w-full border-4 border-white rounded-md">
+            {editIndex === index ? (
+              <div className='flex flex-row items-center justify-end'>
+                <input
+                  type="text"
+                  value={editedText}
+                  onChange={(e) => setEditedText(e.target.value)}
+                />
+                <button
+                  className="text-green-500 ml-2"
+                  onClick={() => updateTodo(index)}
+                >
+                  Save
+                </button>
+                <button
+                  className="text-red-500 ml-2"
+                  onClick={stopEditing}
+                >
+                  Cancle
+                </button>
+              </div>
+            ) : (
+              <div className='flex flex-row items-center justify-end'>
+                <div>
+                  <input
+                    type="checkbox"
+                    className="mr-2"
+                    checked={todo.completed}
+                    onChange={() => toggleTodo(index)}
+                  />
+                  <span className={`${todo.completed ? 'line-through' : ''}`}>
+                    {todo.text}
+                  </span>
+                </div>
+                <div className='flex flex-row gap-4 ml-[13rem]'>
+                  <AiTwotoneEdit size={"25px"} onClick={() => startEditing(index)} />
+                  <AiTwotoneDelete 
+                    onClick={() => removeTodo(index)}
+                    size={'25px'}
+                  />
+                </div>
+              </div>
+            )}
           </li>
         ))}
       </ul>
--- app/title.tsx
+++ app/title.tsx
@@ -7,12 +7,16 @@ interface TitlePageProps {
 
 const TitlePage: React.FC<TitlePageProps> = ({title}) => {
   return (
-    <div className="flex items-center justify-between mb-4">
+    <div>
+      <div className="flex flex-row items-center justify-between mb-4">
         <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">{title}</h5>
         <a href="#" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
-            View all
+          View all
         </a>
+      </div>
+      <div>
         <Todolist />
+      </div>
     </div>
   )
 }
